2480e5a16cdb5a152cb772a09193ebafef962bd3
Sarah Rainsberger <sarah@rainsberger.ca>
2021-03-05 14:54:25 +0000

    Header.js: updated button 1 in List

--- src/components/Header.js
+++ src/components/Header.js
@@ -26,7 +26,7 @@ const Header = props => (
               props.onOpenArticle('intro')
             }}
           >
-            Intro
+            Concerts
           </button>
         </li>
         <li>
