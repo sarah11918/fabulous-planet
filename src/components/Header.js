1c12678523d36bc9701ba955a2298c544bc13c13
Sarah Rainsberger <sarah@rainsberger.ca>
2021-03-05 15:39:20 +0000

    Header.js: updated button 2 in List

--- src/components/Header.js
+++ src/components/Header.js
@@ -29,7 +29,7 @@ const Header = props => (
               props.onOpenArticle('work')
             }}
           >
-            Jon
+            joint
           </button>
         </li>
         <li>
