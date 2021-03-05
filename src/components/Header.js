a422a10035f810259241e568b5cd6aa72c565073
Sarah Rainsberger <sarah@rainsberger.ca>
2021-03-05 14:45:43 +0000

    Header.js: updated Header in div

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ const Header = props => (
     </div>
     <div className="content">
       <div className="inner">
-        <h1>sarah11918</h1>
+        <h1>Summerside Community Choir</h1>
         <p>
           A fully responsive site template designed by{' '}
           <a href="https://html5up.net">HTML5 UP</a> and released
